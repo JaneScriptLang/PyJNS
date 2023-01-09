3a397ffb7160ed32ca52a6b069c35cd7e68c4433
StupidJane <74728802+Stift007@users.noreply.github.com>
2023-01-09 15:37:06 +0100

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,116 +4,39 @@ import os
 class Parser:
     def __init__(self, code):
         self.code = code
-        print(".")
-        self._imports = []
         self.path = [os.getcwd(), os.path.dirname(os.path.abspath(__file__))+"/include"]
         self.parse(self.code)
 
+
     def parse(self, code, ret=False):
-        code = self.parseComments(code)
+        code = self.ParsePragmas(code)
+        #if not hasattr(self, "NI_BUILTINS"):
+        #    code = "#include <__builtins__>"+"\n"+code
+        code = self.ParseComments(code)
         code = self.ParseImports(code)
-        code = self.parsePragmas(code)
+        code = self.ParseEOL(code)
+        code = self.ParseBraces(code)
+        code = self.ParseIndents(code)
         code = self.ParseKeywords(code)
-        code = self.parseBraces(code)
+        code = self.CleanupCode(code)
         if ret:
             return code
         with open("outs.py", "w") as f:
             f.write(code)
 
-    def parseBraces(self, code: str) -> str:
-        leftBracesAmount = 0
+    def ParseComments(self, code):
         for line in code.splitlines():
-            if "{" in line:
-                lineChars = list(line)
-                stringCount = 0
-                for i in range(len(lineChars)):
-                    if lineChars[i] == '"' or lineChars[i] == "'":
-                        stringCount += 1
-                    if lineChars[i] == "{":
-                        if stringCount % 2 == 0 and stringCount != 0:
-                            leftBracesAmount += 1
-                            break
-        rightBracesAmount = 0
-        for line in code.splitlines():
-            if "}" in line:
-                lineChars = list(line)
-                stringCount = 0
-                for i in range(len(lineChars)):
-                    if lineChars[i] == '"' or lineChars[i] == "'":
-                        stringCount += 1
-                    if lineChars[i] == "}":
-                        if stringCount % 2 == 0 and stringCount != 0:
-                            rightBracesAmount += 1
-                            break
-
-        if leftBracesAmount != rightBracesAmount:
-            Error(("Braces amount is not equal"))
-
-        newCode = ""
-        splitLines = code.splitlines();
-        for line in splitLines:
-            if ";" in line and not self.StrContains(";", line):
-                lineChars = list(line)
-                stringCount = 0
-                for i in range(len(lineChars)):
-                    if lineChars[i] == '"' or lineChars[i] == "'":
-                        stringCount += 1
-                    if lineChars[i] == ";":
-                        if stringCount % 2 == 0:
-                            lineChars[i] = "\n"
-                            break
-                line = "".join(lineChars)
-            if "class" in line:
-                if not self.StrContains("class", line):
-                    line = "\n"+" ".join(line.split())
-            if "function" in line:
-                if line.partition("function")[0].count("\"") != 0 and line.partition("function")[0].count("\"") % 2 == 0:
-                    words = line.split()
-                    for wordNo, word in enumerate(words):
-                        if word == "function":
-                            speechCount = line.partition("function")[2].count("\"")
-                            otherCount = line.partition("function")[2].count("'")
-                            if speechCount % 2 == 0 and otherCount % 2 == 0:
-                                words[wordNo] = "def"
-                                break
-                    line = " ".join(words)
-            leftBraceExpression = ''.join(line.split())
-            if not self.StrContains("{", leftBraceExpression):
-                if ''.join(line.split()).startswith(("{")):
-                    newCode += ":\n"
-            if not self.StrContains("}", line):
-                    line = line.replace("}", "#endindent")
-            if not self.StrContains("{", line):
-                line = line.replace("{", "#startindent")
-            line += "\n"
-            newCode += line
-            line += "\n"
-
-        return code
-
-    def parseComments(self, code):
-        for line in code.splitlines():
-            words = line.split()
-            for idx, word in enumerate(words):
-                if word.startswith("//") and not self.StrContains("//", line):
-                    code = code.replace(line, line.split("//")[0])
-        return code
-
-    def parsePragmas(self, code):
-        for line in code.splitlines():
-            words = line.split()
-            for idx, word in enumerate(words):
-                if word == "#pragma" and not self.StrContains("#pragma", line):
-                    pragmaName = words[idx+1]
-                    val = words[idx+2:]
-                    setattr(self, pragmaName, val)
-                    code = code.replace(line, "")
-                elif word == "#parse":
-                    self.parse(line[1:], True)
+            if line.startswith("//"):
+                code = code.replace(line,"\n")
+            if "//" in line and not self.StrContains("//", line):
+                code = code.replace(line, line.split("//")[0])
 
         return code
 
     def ParseKeywords(self, code):
+        """
+        Changing `fn` to `def`
+        """
         for line in code.splitlines():
             words = line.split()
             for idx, word in enumerate(words):
@@ -123,6 +46,13 @@ class Parser:
                 if word == "structure" and not self.StrContains("structure", line):
                     code = code.replace(line,  line.replace("structure", "class"))
                 
+                if word == "for" and not self.StrContains("for", line) and not "#transpiled" in line:
+                    _, var, kw, fn, stp, step = line.split() # Syntax: `for _=0 until 5 step 1`
+                    step = step.partition(":")[0]
+                    varname, initial = var.split("=")
+                    indents = line.count("  ")
+                    ind = "  "*indents
+                    code = code.replace(line, f'for {varname} in range({initial}, {fn}, {step}): #transpiled')
                 if word == "const" and not self.StrContains("const", line):
                     varname = words[idx+1]
 
@@ -130,8 +60,18 @@ class Parser:
                     for l in code.splitlines():
                         if varname in l and not self.StrContains(varname,l):
                             code = code.replace(l, l.replace(varname, varname.upper()))
-            
         return code
+
+    def ParsePragmas(self, code):
+        for line in code.splitlines():
+            words = line.split()
+            for idx, word in enumerate(words):
+                if word == "#pragma" and not self.StrContains("#pragma", line):
+                    
+                    name, val = line[len(word):].split(",")
+                    setattr(self,name,val)
+        return code
+
 
     def ParseImports(self, code):
         for line in code.splitlines():
@@ -151,15 +91,64 @@ class Parser:
                     importName = words[idx+1].strip("<>")
                     for i in self.path:
                         if os.path.exists(i+"/"+importName+".jns"):
-                            if importName in self._imports: continue
                             with open(i+"/"+importName+".jns","r") as f:
                                 nc = self.parse(f.read(), True)
-                                if not nc in code:
-                                    code = code.replace(line, nc)
-                            self._imports.append(importName)
+                                code = nc+"\n"+code
         return code
-    
-    
+
+    def ParseEOL(self, code):
+        for i, line in enumerate(code.splitlines()):
+            if line.startswith("#") or ("{" in line) or "}" in line or not line:
+                continue
+            if line[-1] == ";":
+                line = line[:-1]
+            else:
+                raise SyntaxError(f"Missing Semicolon on line #{i+1}!")
+        return code
+
+    def ParseBraces(self, code):
+        lbraces = 0
+        rbraces = 0
+        code = code.splitlines()
+        for i, line in enumerate(code):
+            if "{" in line and not self.isAssignment(line) and not self.StrContains("{", line):
+                lbraces += 1
+                code[i] =( line.replace("{",":"))
+                code.insert(i+1, "#startindent")
+            if "}" in line and not self.StrContains("}", line):
+                rbraces += 1
+                code[i] =( line.replace("}",""))
+                code.insert(i+1, "#endindent")
+
+        assert lbraces == rbraces, "Opening Brace Count does not match Closing Brace count"
+        return "\n".join(code)
+
+    def isAssignment(self,line):
+        if "{" in line:
+            if self.StrContains("{",line): return True
+            if ('={' in line.replace(" ","").replace("\t","")): return True
+
+        return False
+
+    def ParseIndents(self, code):
+        indentCount = 0
+        code = code.splitlines()
+        for i, line in enumerate(code):
+            if line == "#startindent":
+                indentCount += 1
+            if line == "#endindent":
+                indentCount -= 1
+            code[i] = ("\t"*indentCount +line)
+        return "\n".join(code)
+
+    def CleanupCode(self, code):
+        """
+        Remove Indentation Placeholders, Import the Builtin Library
+        """
+        code = code.replace("#startindent","")
+        code = code.replace("#endindent","")
+        return code
+
     def StrContains(self, pattern, line):
         if not pattern in line: return False
 
